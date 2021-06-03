8cf68cfc6d0eae8282d599b5f1d035be3b7f66f8
Anshul Singh <mail.anshul.singh@gmail.com>
2021-06-03 18:27:23 +0530

    error handled

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const KEY = "AIzaSyD6rsHDc8EV7okktoxHu4ibE9XSI1lYrhU";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
